5d33decc96fe9d3f4a439896ee988c368112ce7d
Shemeer <shameerbem@gmail.com>
2023-05-18 14:24:59 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -76,11 +76,11 @@ function App(){
 
     return(
         <div id="main">
-          <input value={fname} data-testid="input1" onChange={(e)=> setFname(e.target.value)} type="text" placeholder="Enter first name"/>
-          <input value={secname} data-testid="input2" onChange={(e)=> setSecname(e.target.value)} type="text" placeholder="Enter second name"/>
+          <input name="name1" value={fname} data-testid="input1" onChange={(e)=> setFname(e.target.value)} type="text" placeholder="Enter first name"/>
+          <input name="name2" value={secname} data-testid="input2" onChange={(e)=> setSecname(e.target.value)} type="text" placeholder="Enter second name"/>
           <button onClick={check} data-testid="calculate_relationship">Calculate Relationship Future</button>
           <button onClick={clear} data-testid="clear">Clear</button>
-          {flag?<p>Please Enter valid input</p>:null}
+          {flag?<h3>Please Enter valid input</h3>:null}
           {size % 6 ==1?<h3>Friends</h3>:size%6 == 2?<h3>Love</h3>:size % 6 ==3?<h3>Affection</h3>:size % 6 ==4?<h3>Marriage</h3>:size % 6 ==5?<h3>Enemy</h3>:size % 6 ==0?<h3>Siblings</h3>:null}
         </div>)
 }
