1f68f0561f78bb88ce94adb6d1fa16dfecb258c4
Shemeer <shameerbem@gmail.com>
2023-05-18 23:09:45 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -80,8 +80,8 @@ function App(){
           <input name="name2" value={secname} data-testid="input2" onChange={(e)=> setSecname(e.target.value)} type="text" placeholder="Enter second name"/>
           <button onClick={check} data-testid="calculate_relationship">Calculate Relationship Future</button>
           <button onClick={clear} data-testid="clear">Clear</button>
-          {flag?<h3>Please Enter valid input</h3>:null}
-          {size % 6 ==1?<h3>Friends</h3>:size%6 == 2?<h3>Love</h3>:size % 6 ==3?<h3>Affection</h3>:size % 6 ==4?<h3>Marriage</h3>:size % 6 ==5?<h3>Enemy</h3>:size % 6 ==0?<h3>Siblings</h3>:null}
+          {/* {flag?<h3>Please Enter valid input</h3>:null} */}
+          {flag?<h3>Please Enter valid input</h3>:size % 6 ==1?<h3>Friends</h3>:size%6 == 2?<h3>Love</h3>:size % 6 ==3?<h3>Affection</h3>:size % 6 ==4?<h3>Marriage</h3>:size % 6 ==5?<h3>Enemy</h3>:size % 6 ==0?<h3>Siblings</h3>:null}
         </div>)
 }
 
